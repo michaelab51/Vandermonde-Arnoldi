814db3aae2e20f6129a00e976841386d24624de0
michaelab51 <144811925+michaelab51@users.noreply.github.com>
2025-04-28 13:13:28 +0200

    Update Cebysev_Lanczos.m

--- Cebysev_Lanczos.m
+++ Cebysev_Lanczos.m
@@ -72,8 +72,8 @@ error = abs(y - y_exact);
 % Grafické zobrazení
 figure;
 subplot(2,1,1);
-plot(s, y_exact, 'k', 'LineWidth', 2); hold on;
-plot(s, y, 'b', 'LineWidth', 2);
+plot(s, y_exact, 'k', 'LineWidth', 3); hold on;
+plot(s, y, 'm--', 'LineWidth', 3);
 title('Interpolation in Chebyshev Points (Lanczos)');
 xlabel('x');
 ylabel('p(x)');
@@ -81,9 +81,9 @@ legend('Exact function', 'Interpolant');
 grid on;
 
 subplot(2,1,2);
-semilogy(s, error, 'r', 'LineWidth', 2);
+semilogy(s, error, 'r', 'LineWidth', 3);
 xlim([0 1]);
 title('Interpolation Error (Lanczos)');
 xlabel('x');
-ylabel('|p(x) - f(x)|');
+ylabel('||p(x) - f(x)||_2');
 grid on;
