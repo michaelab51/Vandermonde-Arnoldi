3233bb6c0ea27f9a09b022ef3b47bfee21e74dc8
michaelab51 <144811925+michaelab51@users.noreply.github.com>
2025-06-26 21:00:43 +0200

    Update klasicky_sinxcos3x.m

--- klasicky_sinxcos3x.m
+++ klasicky_sinxcos3x.m
@@ -1,14 +1,3 @@
-function c = polyfit(x, f, n)
-    A = x .^ (0:n);
-    c = A \ f;
-end
-
-function y = polyval(c, s)
-    n = length(c) - 1;
-    B = s .^ (0:n);
-    y = B * c;
-end
-
 % Diskretizace domény
 x = linspace(-pi, pi, 1000)';  % Husté body pro přesnou aproximaci
 
