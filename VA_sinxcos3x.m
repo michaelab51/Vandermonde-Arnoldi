c02da391ba3b63b0e8903f581534d14fad4822b2
michaelab51 <144811925+michaelab51@users.noreply.github.com>
2025-06-26 20:58:56 +0200

    Update VA_sinxcos3x.m

--- VA_sinxcos3x.m
+++ VA_sinxcos3x.m
@@ -1,33 +1,3 @@
-function [d,H] = polyfitA(x, f, n)
-    m = length(x);
-    Q = ones(m,1);
-    H = zeros(n+1, n);
-    for k = 1:n
-        q = x .* Q(:,k);
-        for j = 1:k
-            H(j,k) = (Q(:,j)' * q) / m;
-            q = q - H(j,k) * Q(:,j);
-        end
-        H(k+1,k) = norm(q) / sqrt(m);
-        Q = [Q q / H(k+1,k)];
-    end
-    d = Q \ f;
-end
-
-function y = polyvalA(d, H, s)
-    M = length(s);
-    W = ones(M,1);
-    n = size(H,2);
-    for k = 1:n
-        w = s .* W(:,k);
-        for j = 1:k
-            w = w - H(j,k) * W(:,j);
-        end
-        W = [W w / H(k+1,k)];
-    end
-    y = W * d;
-end
-
 % Generování dat
 x = linspace(0, 2*pi, 1000)';
 f = sin(x) .* cos(3*x); % cílová funkce
