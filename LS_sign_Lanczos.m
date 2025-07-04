20326b279378ccbfc506d9a71d28aee54a22eeba
michaelab51 <144811925+michaelab51@users.noreply.github.com>
2025-07-04 22:52:56 +0200

    Update LS_sign_Lanczos.m

--- LS_sign_Lanczos.m
+++ LS_sign_Lanczos.m
@@ -1,4 +1,4 @@
-% Diskretizace domény pro least squares problém
+% Diskretizace domény
 n = 80;
 x1 = linspace(-1, -1/3, 500)';
 x2 = linspace(1/3, 1, 500)';
@@ -12,9 +12,9 @@ f = sign(x);
 
 % Evaluace výsledného polynomu
 s = linspace(-1, 1, 1000)';
-y = polyvalA(c, T, s);
+y = polyvalA(c, T, s, m);
 
-% Výpočet skutečné hodnoty funkce sign(x)
+% Výpočet skutečné hodnoty funkce
 sign_x = sign(s);
 error = abs(y - sign_x);
 
@@ -23,16 +23,18 @@ figure;
 subplot(2,1,1);
 plot(s, sign_x, 'k', 'LineWidth', 3); hold on;
 plot(s, y, 'm--', 'LineWidth', 3);
-title('Aproximace funkce sign(x) (Lanczos)');
-xlabel('x');
-ylabel('p(x)');
-legend('sign(x)', 'Approximace');
+title('Aproximace funkce sign(x) (Lanczos)', 'FontSize', 16);
+xlabel('x', 'FontSize', 14);
+ylabel('p(x)', 'FontSize', 14);
+legend('sign(x)', 'Approximace', 'Location', 'southeast', 'FontSize', 12);
 grid on;
+set(gca, 'FontSize', 12);
 
 subplot(2,1,2);
 plot(s, error, 'r', 'LineWidth', 3);
-xlim([0 1]);
-title('Aproximační chyba');
-xlabel('x');
-ylabel('||p(x) - sign(x)||_2');
+xlim([-1 1]);
+title('Aproximační chyba', 'FontSize', 16);
+xlabel('x', 'FontSize', 14);
+ylabel('|p(x) - sign(x)|', 'FontSize', 14);
 grid on;
+set(gca, 'FontSize', 12);
